e882e21473f5d0604f489ca4a966f2a1bc076330
manuchassot <manu.chassot@gmail.com>
2022-01-10 12:22:34 +0400

    addition of complementary data sets

--- initialization/02_DATA_READING.R
+++ initialization/02_DATA_READING.R
@@ -2,10 +2,28 @@
 # Data collected as per EU (DCF and EUMAP) ####
 
 ## Indian Ocean ####
-IO_RAW_SAMPLES_WITH_ENVIRONMENT = fread("../inputs/data/AAD IND Chassot 2021.txt")[!is.na(geometry) & geometry != "NA" & ocean_code == "IO"]
+IO_RAW_SAMPLES_WITH_ENVIRONMENT = fread("../inputs/data/AAD IND Chassot 2021.txt")
+
+# Update ocean_code when missing and information shows the fish is from the Indian Ocean
+IO_RAW_SAMPLES_WITH_ENVIRONMENT[is.na(ocean_code) & vessel_name == "PLAYA DE ANZORAS", ocean_code := "IO"]
+
+# Convert NA character to NA
+IO_RAW_SAMPLES_WITH_ENVIRONMENT[geometry == "NA", geometry := NA]
+
+# No information on origin of the fish
+IO_INDIC_NO_OCEAN_INFORMATION = is.na(IO_RAW_SAMPLES_WITH_ENVIRONMENT$ocean_code)
+IO_RAW_SAMPLES_WITH_ENVIRONMENT1 = IO_RAW_SAMPLES_WITH_ENVIRONMENT[!IO_INDIC_NO_OCEAN_INFORMATION]
+
+# Ocean code different than IO
+IO_INDIC_OTHER_OCEAN = IO_RAW_SAMPLES_WITH_ENVIRONMENT1$ocean_code != "IO"
+IO_RAW_SAMPLES_WITH_ENVIRONMENT2 = IO_RAW_SAMPLES_WITH_ENVIRONMENT1[!IO_INDIC_OTHER_OCEAN]
+
+# No geometry
+IO_INDIC_NO_GEOMETRY = is.na(IO_RAW_SAMPLES_WITH_ENVIRONMENT2$geometry)
+IO_RAW_SAMPLES_WITH_ENVIRONMENT3 = IO_RAW_SAMPLES_WITH_ENVIRONMENT2[!IO_INDIC_NO_GEOMETRY]
 
 ## Atlantic Ocean ####
-AO_RAW_SAMPLES_WITH_ENVIRONMENT = fread("../inputs/data/AAD ATL Chassot 2021.txt")[!is.na(geometry)]
+AO_RAW_SAMPLES_WITH_ENVIRONMENT = fread("../inputs/data/AAD ATL Chassot 2021.txt")#[!is.na(geometry)]
 
 # TEMP
 # Replace ";" by " " in geometry
@@ -17,4 +35,26 @@ AO_RAW_SAMPLES_WITH_ENVIRONMENT[indic_comma, geometry := gsub(",", ".", geometry
 
 # Note: error in geometries (fish from WPO)
 #POLYGON ((108 15. 115 15.115 20.-175.20.-175 -25.155 -25.155 -28.15.153.57 -28.15.105.82 -6.38.105.07 -6.25.104.5 -5.52))
+
+# No information on origin of the fish
+AO_INDIC_NO_OCEAN_INFORMATION = is.na(AO_RAW_SAMPLES_WITH_ENVIRONMENT$ocean_code)
+AO_RAW_SAMPLES_WITH_ENVIRONMENT1 = AO_RAW_SAMPLES_WITH_ENVIRONMENT[!AO_INDIC_NO_OCEAN_INFORMATION]
+
+# Other data sets ####
+
+IO_EMOTION   = fread("../inputs/data/data_emotion_fl_wt_2009_2014.csv")[c_sp_fao %in% c("BET", "SKJ", "YFT")]
+names(IO_EMOTION) = c("project", "sampling_year", "species_code_fao", "sex", "fork_length", "whole_fish_weight")
+
+IO_FONTENEAU = fread("../inputs/data/data_fonteneau_fl_wt_2003.csv")
+names(IO_FONTENEAU) = c("project", "sampling_year", "species_code_fao", "sex", "fork_length", "whole_fish_weight")
+
+IO_OTHERS    = fread("../inputs/data/data_iotmdm_fl_wt_1986_2015.csv")[project != "DCF"]
+names(IO_OTHERS) = c("project", "sampling_year", "species_code_fao", "sex", "fork_length", "whole_fish_weight")
+
+IO_IOTTP     = fread("../inputs/data/data_iottp_fl_wt_2009_2015.csv")
+names(IO_IOTTP) = c("project", "sampling_year", "species_code_fao", "sex", "fork_length", "whole_fish_weight")
+
+IO_PROJECTS = rbindlist(list(IO_EMOTION, IO_FONTENEAU, IO_OTHERS, IO_IOTTP), fill = T)
+
+
 
--- initialization/03_DATA_CONSOLIDATION.R
+++ initialization/03_DATA_CONSOLIDATION.R
@@ -1,9 +1,12 @@
 
 # Combine both data sets
-RAW_SAMPLES_WITH_ENVIRONMENT = rbindlist(list(AO_RAW_SAMPLES_WITH_ENVIRONMENT, IO_RAW_SAMPLES_WITH_ENVIRONMENT))
+RAW_SAMPLES_WITH_ENVIRONMENT = rbindlist(list(AO_RAW_SAMPLES_WITH_ENVIRONMENT1, IO_RAW_SAMPLES_WITH_ENVIRONMENT3))
 
 # Format sampling date
 RAW_SAMPLES_WITH_ENVIRONMENT[, fish_sampling_date := as.POSIXct(fish_sampling_date)] 
+
+# Add sampling year
+RAW_SAMPLES_WITH_ENVIRONMENT[, sampling_year := year(fish_sampling_date)]
 
 # Format landing date
 RAW_SAMPLES_WITH_ENVIRONMENT[, landing_date := as.POSIXct(landing_date)]
@@ -19,32 +22,49 @@ RAW_SAMPLES_WITH_ENVIRONMENT[, fishing_date_min := as.POSIXct(fishing_date_min)]
 RAW_SAMPLES_WITH_ENVIRONMENT[, fishing_date_max := as.POSIXct(fishing_date_max)]
 RAW_SAMPLES_WITH_ENVIRONMENT[, fishing_date := as.POSIXct(fishing_date)]
 
-# Update fishing date
+# Update fishing date with average fishing date when missing
+RAW_SAMPLES_WITH_ENVIRONMENT[is.na(fishing_date) & !is.na(fishing_date_min)] 
+
 RAW_SAMPLES_WITH_ENVIRONMENT[!is.na(fishing_date), `:=` (fishing_date_min = NA, fishing_date_max = NA)]
 
 # Compute "average" date when only min and max are available
-RAW_SAMPLES_WITH_ENVIRONMENT[is.na(fishing_date), fishing_date_avg := format(fishing_date_min + difftime(fishing_date_max, fishing_date_min, units = 'days')/2, "%Y-%m-%d")]
+RAW_SAMPLES_WITH_ENVIRONMENT[is.na(fishing_date), fishing_date_avg := as.Date(fishing_date_min + difftime(fishing_date_max, fishing_date_min, units = "days")/2, format = "%Y-%m-%d")]
 
-RAW_SAMPLES_WITH_ENVIRONMENT[!is.na(fishing_date), fishing_date_avg := format(min(fishing_date) + difftime(max(fishing_date), min(fishing_date), units = 'days')/2, "%Y-%m-%d"), by = .(fish_identifier)]
+RAW_SAMPLES_WITH_ENVIRONMENT[!is.na(fishing_date), fishing_date_avg := as.POSIXct(min(fishing_date) + difftime(max(fishing_date), min(fishing_date), units = 'days')/2, format = "%Y-%m-%d"), by = .(fish_identifier)]
+
+RAW_SAMPLES_WITH_ENVIRONMENT[, fishing_date_avg := as.POSIXct(fishing_date_avg)]
 
 # Add fishing date range
-RAW_SAMPLES_WITH_ENVIRONMENT[!is.na(fishing_date), fishing_date_range := ]
+RAW_SAMPLES_WITH_ENVIRONMENT[is.na(fishing_date), fishing_date_range := as.numeric(difftime(fishing_date_max, fishing_date_min, units = "days"))]
+RAW_SAMPLES_WITH_ENVIRONMENT[!is.na(fishing_date), fishing_date_range := as.numeric(difftime(max(fishing_date), min(fishing_date), units = "days")), by = .(fish_identifier)]
 
 # Update vessel storage mode
 RAW_SAMPLES_WITH_ENVIRONMENT[vessel_storage_mode == "brine", vessel_storage_mode := "Brine"]
 RAW_SAMPLES_WITH_ENVIRONMENT[vessel_name %in% c("ARTZA", "CAP SAINTE MARIE", "GLENAN", "TORRE ITALIA"), vessel_storage_mode := "Brine"]
+RAW_SAMPLES_WITH_ENVIRONMENT[is.na(vessel_storage_mode), vessel_storage_mode := "Unknow"]
 
-
+# Update devices
+RAW_SAMPLES_WITH_ENVIRONMENT[measuring_device_1 == "Calliper", measuring_device_1 := "calliper"]
+RAW_SAMPLES_WITH_ENVIRONMENT[measuring_device_2 == "Calliper", measuring_device_2 := "calliper"]
 
 # Rename headers
-SAMPLES_WITH_ENVIRONMENT = RAW_SAMPLES_WITH_ENVIRONMENT[, .(FISH_ID = fish_identifier, SAMPLING_DATE = fish_sampling_date, PROJECT = project, SPECIES_CODE = species_code_fao, PDL_DEVICE = measuring_device_1, PDL = first_dorsal_length, FL_DEVICE = measuring_device_2, FL = fork_length, RW_device = measuring_device_4, RW = whole_fish_weight, SEX = sex, GEAR_CODE = gear_code, SCHOOL_TYPE = fishing_mode, FISHING_DATE = fishing_date, GEOM = geometry)]
+#SAMPLES = unique(RAW_SAMPLES_WITH_ENVIRONMENT[, .(FISH_ID = fish_identifier, SAMPLING_DATE = fish_sampling_date, PROJECT = project, SPECIES_CODE = species_code_fao, FL = fork_length, FL_DEVICE = measuring_device_1, SF = first_dorsal_length, SF_DEVICE = measuring_device_2,  RD = whole_fish_weight, RD_device = measuring_device_4, SEX = sex, GEAR_CODE = gear_code, sCHOOL_TYPE = fifelse(length(unique(fishing_mode)) >1, "MIX", unique(fishing_mode)), FISHING_DATE_AVG = fishing_date_avg)])
 
+# Identify id replicates
+ID_REPLICATES = unique(RAW_SAMPLES_WITH_ENVIRONMENT[, .(fish_identifier, species_code_fao, fork_length, first_dorsal_length, whole_fish_weight, gutted_fish_weight, measuring_device_1, measuring_device_2, measuring_device_4)])[, .N, keyby = fish_identifier][N>1, fish_identifier]
 
+SAMPLES_VARIABLES = RAW_SAMPLES_WITH_ENVIRONMENT[!fish_identifier %in% ID_REPLICATES, .(FISHING_DATE_AVG = format(min(fishing_date_avg) + difftime(max(fishing_date_avg), min(fishing_date_avg), units = 'days')/2, "%Y-%m-%d")), keyby = .(FISH_ID = fish_identifier, SAMPLING_DATE = fish_sampling_date, PROJECT = project, SPECIES_CODE = species_code_fao, FL = fork_length, FL_DEVICE = measuring_device_1, SF = first_dorsal_length, SF_DEVICE = measuring_device_2,  RD = whole_fish_weight, RD_device = measuring_device_4, SEX = sex, GEAR_CODE = gear_code)]
 
+# .(SCHOOL_TYPE = fifelse(length(unique(fishing_mode)) == 1, unique(fishing_mode), "MIX"))
+# SCHOOL_TYPE
+# FISHING_DATE_AVG
+# FISHING_DATE_MIN
+# FISGING_DATE_MAX
 
 # Convert to simple feature
-RAW_SAMPLES_WITH_ENVIRONMENT_SF = st_as_sf(RAW_SAMPLES_WITH_ENVIRONMENT, wkt = "geometry", crs = 4326)
+RAW_SAMPLES_WITH_ENVIRONMENT_SF = st_as_sf(RAW_SAMPLES_WITH_ENVIRONMENT[1:1000], wkt = "geometry", crs = 4326)
 
+RAW_SAMPLES_WITH_ENVIRONMENT_SF %>% group_by(FISH_ID)
 
 toto1_SF = st_as_sf(toto1, wkt = "GEOM", crs = 4326)
 
