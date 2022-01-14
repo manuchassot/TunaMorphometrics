80c02af727d82918ca9bab16057ff44c6001371f
manuchassot <manu.chassot@gmail.com>
2022-01-14 14:40:08 +0400

    progress in Bayesian model

---
+++ initialization/07_FORK_LENGTH_ROUND_WEIGHT_MODELS_BAYESIAN.R
@@ -0,0 +1,111 @@
+
+# BAYESIAN MODEL ####
+nls2_jags <- function(){
+  
+  # Priors
+  log10a_prior ~ dnorm(-4.589, 21)   # intercept prior
+  log10a ~ dnorm(-4.589, 21)         # intercept
+  b_prior      ~ dnorm(2.955, 91)    # slope prior
+  b            ~ dnorm(2.955, 91)    # slope
+  sigma_prior ~ dunif(0, 100)        # model error standard deviation prior
+  sigma       ~ dunif(0, 100)        # standard deviation (both measurement and residual combined) - Uniform between 0 and 100
+  
+  # Likelihood
+  for (i in 1:N){
+    log10wt[i] ~ dnorm(mean_log10wt[i], tau)
+    mean_log10wt[i] = log10a + b * log10fl[i]
+  }
+  
+  # Derived quantities
+  tau     = pow(sigma, -2)                  # tau is precision (1 / variance)
+  a       = 10^(log10a) * 10^(sigma^2)/2    # a with bias correction
+  a_prior = 10^(log10a_prior)
+  a1e5_prior = 10^(log10a_prior) * 1e5
+  a1e5   = a*1e5
+    
+  # Assess model using a sums-of-squares-type discrepancy
+  for (i in 1:N){
+    residual[i]  = log10wt[i] - mean_log10wt[i]    # Residuals for observed data
+    predicted[i] = mean_log10wt[i]                 # Predicted values
+    sq[i]        = pow(residual[i], 2)             # Square residuals for observed data
+    
+    # Generate replicate data and compute fit stats for them
+    log10wt.new[i] ~ dnorm(mean_log10wt[i], tau)              # One new data set at each MCMC iteration
+    sq.new[i]    = pow(log10wt.new[i] - predicted[i], 2)      # Squared residuals for new data
+  }
+  
+  # Quantities of interest
+  fit     = sum(sq[])           # Sum of square residuals for actual data set
+  fit.new = sum(sq.new[])       # Sum of squared residuals for new data set
+  test    = step(fit.new - fit) # Test whether new data set more extreme
+  bpvalue = mean(test)          # Bayesian p-value
+}
+
+## DATA ####
+FORK_LENGTH_ROUND_WEIGHT_DATASET = FULL_DATASET[ocean_code == "IO" & species_code_fao == "BET" & !is.na(fork_length) & !is.na(whole_fish_weight)]
+
+JAGS_DATA = with(FORK_LENGTH_ROUND_WEIGHT_DATASET, list(log10fl = log(fork_length)/log(10), log10wt = log(whole_fish_weight)/log(10), N = FORK_LENGTH_ROUND_WEIGHT_DATASET[, .N]))
+
+## PARAMETERS TO ESTIMATE ####
+params = c("log10a_prior", "log10a", "b_prior", "b", "sigma", "sigma_prior", "a", "a_prior", "a1e5", "a1e5_prior", "fit", "fit.new", "bpvalue", "residual", "predicted")
+
+## INITIALIZATION FUNCTION ####
+init_values = function(){
+  list(log10a = rnorm(1, mean = 0, sd = 1), b = runif(1, 1, 6), sigma = runif(1, min = 0, max = 1))
+}
+
+# STATISTICAL INFERENCE ####
+### 3 chains of 12,000 samples each, remove 2,000 first and extract every 10 sample = 1,000 samples per chain x 3 = 3,000 samples
+
+FL_RW_MODEL = jags(data = JAGS_DATA, inits = init_values, parameters.to.save = params, model.file = nls2_jags, n.chains = 3, n.iter = 50000, n.burnin = 2000, n.thin = 10, DIC = T)
+
+# OUTPUTS ####
+
+ITERATIONS = FL_RW_MODEL$BUGSoutput$n.sims
+
+PARAMETERS = c("a1e5_prior", "a1e5", "b_prior", "b", "sigma_prior", "sigma")
+
+PARAMETERS_POSTERIORS = rbindlist(lapply(FL_RW_MODEL$BUGSoutput$sims.list[PARAMETERS], as.data.frame))
+
+names(PARAMETERS_POSTERIORS)[1] = "VALUE"
+
+PARAMETERS_POSTERIORS[, PARAM := rep(PARAMETERS, each = ITERATIONS)]
+
+PARAMETERS_POSTERIORS[, PARAM := factor(PARAM, levels = c("a1e5_prior", "b_prior", "sigma_prior", "a1e5", "b", "sigma"))]
+
+setcolorder(PARAMETERS_POSTERIORS, c("PARAM", "VALUE"))
+
+## PRIOR & POSTERIOR PARAMETERS ####
+
+ggplot(data = PARAMETERS_POSTERIORS, aes(x = VALUE)) +
+  geom_histogram(aes(y = stat(count) / sum(count)), bins = 30, fill = "grey", color = "black", alpha = 0.6) +
+  theme_bw() +
+  labs(x = "", y = "Relative frequency") +
+  facet_wrap(~PARAM, scale = "free", ) +
+  theme(strip.background = element_rect(fill = "white"))
+
+### PREDICTIONS ####
+
+### Extract mean values of alpha & beta
+mean_predictions <- exp(as.numeric(fit_nls2$BUGSoutput$mean$alpha) + as.numeric(fit_nls2$BUGSoutput$mean$beta) * JAGS_DATA$logfl)
+
+### Array of predictions
+predictions <- array(dim = c(JAGS_DATA$N, fit_nls2$BUGSoutput$n.sims))
+
+for (i in 1:JAGS_DATA$N){
+  predictions[i,] <-  exp(fit_nls2$BUGSoutput$sims.list$alpha + fit_nls2$BUGSoutput$sims.list$beta * JAGS_DATA$logfl[i])
+}
+
+# Lower bound
+LPB <- apply(predictions, 1, quantile, 0.025, na.rm = TRUE)   #2.5% quantile for each observation logFL
+UPB <- apply(predictions, 1, quantile, 0.975, na.rm = TRUE)  #97.5% quantile for each observation logFL
+
+### Plot the observations
+plot(exp(JAGS_DATA$logfl), exp(JAGS_DATA$logwt), cex.axis = 1.3, cex.lab = 1.3, las = 1, xlab = expression(paste("Fork length ",italic(L[F])," (cm)",sep="")), ylab = expression(paste("Round weight ", italic(W[T])," (kg)",sep="")), pch = 19, col = "darkgrey", cex = 0.3)
+
+### Add the mean predictions
+lines(sort(exp(JAGS_DATA$logfl)), sort(mean_predictions),col="black",lwd=2)
+
+### Add the credible interval (uncertainty interval from the actual data)
+lines(sort(exp(JAGS_DATA$logfl)), sort(LPB), col = "red", lty = 2)
+lines(sort(exp(JAGS_DATA$logfl)), sort(UPB), col = "red", lty = 2)
