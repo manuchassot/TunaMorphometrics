136bc35af7eabbaa03a0fd29e2ff1f458e0127ea
manuchassot <manu.chassot@gmail.com>
2022-02-22 19:16:11 +0400

    improved cleaning of the data

--- initialization/03_DATA_CONSOLIDATION.R
+++ initialization/03_DATA_CONSOLIDATION.R
@@ -81,46 +81,16 @@ TUNA_SAMPLES = TUNA_SAMPLES[project != "ITOP", ]
 # Select only tunas caught with purse seine
 TUNA_SAMPLES = TUNA_SAMPLES[gear_code == "PS", ]
 
-# Obvious errors in IO SKJ weights
-TUNA_SAMPLES[organism_identifier %in% c("SKJ_20210812_21", "SKJ_20210727_17", "IOT_2016_0405", "IOT_2016_0422", "IOT_2974", "IOT_2454", "IOT2014-016", "IOT_0247", "IOT_3363", "IOT_3366", "IOT_3672", "IOT_3755", "SKJ-09.M1.L17.7", "SKJ-09.M1.L25.23", "SKJ-09.M2.L10.16", "IOT_2938", "SA01893", "DCF-AO-1280", "SKJ-09.M3.L22.4", "IOT_2016_0414", "IOT_3460", "SKJ_20210916_17", "SKJ_20210916_39", "IOT_3128", "IOT_0588", "IOT_3685", "SKJ_20210824_30", "SKJ_20210824_4", "SKJ_20210907_19", "SKJ_20210826_1", "IOT_2016_0416", "IOT_2016_0423", "IOT_2016_0431", "IOT_1033", "SKJ_20210916_31", "IOT_0613", "SKJ_20210701_1", "IOT_2016_0442", "IOT_2494", "IOT_2508", "IOT_2016_0420", "IOT_2016_0458", "IOT_2016_0488", "IOT_0481", "IOT_3365", "IOT_2490"), whole_weight_kg := NA]
+# Obvious errors in weights
+TUNA_SAMPLES[organism_identifier %in% c("SKJ_20210812_21", "SKJ_20210727_17", "IOT_2016_0405", "IOT_2016_0422", "IOT_2974", "IOT_2454", "IOT2014-016", "IOT_0247", "IOT_3363", "IOT_3366", "IOT_3672", "IOT_3755", "SKJ-09.M1.L17.7", "SKJ-09.M1.L25.23", "SKJ-09.M2.L10.16", "IOT_2938", "SA01893", "DCF-AO-1280", "SKJ-09.M3.L22.4", "IOT_2016_0414", "IOT_3460", "SKJ_20210916_17", "SKJ_20210916_39", "IOT_3128", "IOT_0588", "IOT_3685", "SKJ_20210824_30", "SKJ_20210824_4", "SKJ_20210907_19", "SKJ_20210826_1", "IOT_2016_0416", "IOT_2016_0423", "IOT_2016_0431", "IOT_1033", "SKJ_20210916_31", "IOT_0613", "SKJ_20210701_1", "IOT_2016_0442", "IOT_2494", "IOT_2508", "IOT_2016_0420", "IOT_2016_0458", "IOT_2016_0488", "IOT_0481", "IOT_3365", "IOT_2490", "SKJ_20210701_14", "AA1958", "DCF-AO-1107", "DCF-AO-1372", "DCF6878", "IOT24", "OT11610", "A260", "YFT-09.M2.L5.4","IOT_2016_0305", "YFT-09.M2.L4.12", "DCF0031", "DCF18985", "DCF_18985"), whole_weight_kg := NA]
 
-# Obvious errors in IO SKJ
-TUNA_SAMPLES[organism_identifier %in% c("SKJ_20210701_14"), whole_weight_kg:= NA]
+# Obvious errors in fork length 
+TUNA_SAMPLES[organism_identifier %in% c("SA0031", "SA01665", "SKJ_20210503_11", "SA0904", "SA0624", "SA0782", "SA0773", "SA0777", "SA01132", "SA01034", "IOT_5065", "IOT_5066", "IOT_5067", "IOT_5011", "IOT_4675", "IOT_4871", "IOT_4585"), fork_length := NA]
 
-TUNA_SAMPLES[organism_identifier == "SA0031", first_dorsal_length := NA]
-TUNA_SAMPLES[organism_identifier == "SA01665", first_dorsal_length := NA]
-TUNA_SAMPLES[organism_identifier == "SKJ_20210503_11", first_dorsal_length := NA]
-TUNA_SAMPLES[organism_identifier == "SA0904", first_dorsal_length := NA]
-TUNA_SAMPLES[organism_identifier == "SA0624", first_dorsal_length := NA]
-TUNA_SAMPLES[organism_identifier == "SA0782", first_dorsal_length := NA]
-TUNA_SAMPLES[organism_identifier == "SA0773", first_dorsal_length := NA]
-TUNA_SAMPLES[organism_identifier == "SA0777", first_dorsal_length := NA]
-
-TUNA_SAMPLES[organism_identifier == "SKJ_20210701_14", first_dorsal_length := 19] #assumed instead of 29
-
-TUNA_SAMPLES[organism_identifier == "SA01132", fork_length := NA]
-
-TUNA_SAMPLES[organism_identifier == "SA01034", fork_length := NA]
-
-TUNA_SAMPLES[organism_identifier == "IOT_5065", fork_length := NA]  # very small FL
-TUNA_SAMPLES[organism_identifier == "IOT_5066", fork_length := NA]  # very small FL
-TUNA_SAMPLES[organism_identifier == "IOT_5067", fork_length := NA]  # very small FL
-TUNA_SAMPLES[organism_identifier == "IOT_5011", fork_length := NA]  # inconsistent
-TUNA_SAMPLES[organism_identifier == "IOT_4675", fork_length := NA]  # inconsistent
-TUNA_SAMPLES[organism_identifier == "IOT_4871", fork_length := NA]  # inconsistent
-TUNA_SAMPLES[organism_identifier == "IOT_4585", fork_length := NA]  # inconsistent
-
-# Obvious errors in AO YFT
-TUNA_SAMPLES[organism_identifier == "DCF-AO-25", first_dorsal_length := 35.5]    # instead of 25.5 assuming round weight is correct
-TUNA_SAMPLES[organism_identifier == "DCF-AO-414", first_dorsal_length := 41]    # instead of 31 assuming round weight is correct
-TUNA_SAMPLES[organism_identifier == "AA1958", whole_weight_kg := NA]    # weight is wrong (64.2?)
-
-# Obvious errors in AO SKJ weights
-TUNA_SAMPLES[organism_identifier == "DCF-AO-1107", whole_weight_kg := NA]
-TUNA_SAMPLES[organism_identifier == "DCF-AO-1372", whole_weight_kg := NA]
-
-# Obvious errors in IO YFT weights
-TUNA_SAMPLES[organism_identifier %in% c("DCF6878", "IOT24", "OT11610", "A260", "YFT-09.M2.L5.4","IOT_2016_0305", "YFT-09.M2.L4.12", "DCF0031", "DCF18985", "DCF_18985"), whole_weight_kg := NA]
+# Obvious errors in first dorsal length
+TUNA_SAMPLES[organism_identifier == "DCF-AO-25", first_dorsal_length := 35.5] # instead of 25.5
+TUNA_SAMPLES[organism_identifier == "DCF-AO-414", first_dorsal_length := 41]  # instead of 31
+TUNA_SAMPLES[organism_identifier == "SKJ_20210701_14", first_dorsal_length :=  19] #instead of 29
 
 # Prepare the data for modelling
 TUNA_SAMPLES[, log10FL  := log(fork_length, 10)]
